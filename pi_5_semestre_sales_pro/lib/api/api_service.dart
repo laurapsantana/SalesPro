7dcfb167a3350439f99cd2937dc2f61f392507a4
Laura Santana <111099291+laurapsantana@users.noreply.github.com>
2024-11-26 23:06:30 -0300

    Update api_service.dart
    Atualizando novos métodos para transmitir os dados nos gráficos

--- pi_5_semestre_sales_pro/lib/api/api_service.dart
+++ pi_5_semestre_sales_pro/lib/api/api_service.dart
@@ -2,65 +2,168 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  final String baseUrl = "http://localhost:3000/produtos"; // URL do servidor da API
+  final String baseUrl = "http://10.0.2.2:3000/"; // Para emulador Android
 
-  Future<double> fetchReceitaLiquida() async {
+  Future<List<VendaMensal>> fetchVendasMensais(int mesSelecionado) async {
+    final response = await http.get(Uri.parse('http://10.0.2.2:3000/venda-mensal/$mesSelecionado'));
+
+    if (response.statusCode == 200) {
+      List jsonResponse = json.decode(response.body);
+      return jsonResponse.map((data) => VendaMensal.fromJson(data)).toList();
+    } else {
+      throw Exception('Falha ao carregar dados de vendas mensais');
+    }
+  }
+
+  // Função para buscar dados de vendas da API
+  Future<void> fetchSalesData() async {
+    final response = await http.get(Uri.parse('http://localhost:3000/analise/vendas'));
+    if (response.statusCode == 200) {
+      final List data = json.decode(response.body);
+    } else {
+      throw Exception('Falha ao carregar os dados');
+    }
+  }
+
+  // Função para buscar as vendas por cidade
+  Future<List<VendasPorCidade>> fetchVendasPorCidade() async {
+    final response = await http.get(Uri.parse('http://10.0.2.2:3000/desempenho-por-cidade'));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      return data.map((json) => VendasPorCidade.fromJson(json)).toList();
+    } else {
+      throw Exception('Erro ao carregar vendas por cidade');
+    }
+  }
+
+  Future<List<Map<String, dynamic>>> fetchDadosPizza() async {
     try {
-      final response = await http.get(Uri.parse("$baseUrl/receita-liquida"));
+      final response = await http.get(Uri.parse('http://10.0.2.2/dados-pizza'));
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body) as List<dynamic>;
+        return data.map((item) {
+          return {
+            'label': item['label'],
+            'value': item['value'],
+          };
+        }).toList();
+      } else {
+        throw Exception('Erro ao buscar os dados de pizza. Código: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Erro ao conectar-se à API: $e');
+    }
+  }
+
+  Future<List<String>> fetchCidades() async {
+    final response = await http.get(Uri.parse('http://10.0.2.2:3000/vendas/por-cidade'));
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+
+      if (data is Map<String, dynamic>) {
+        final maisVenderam = List<Map<String, dynamic>>.from(data['cidadesQueMaisVenderam']);
+        final menosVenderam = List<Map<String, dynamic>>.from(data['cidadesQueMenosVenderam']);
+
+        // Concatena as cidades de mais e menos vendas, eliminando duplicadas
+        final todasCidades = [
+          ...maisVenderam.map((c) => c['cidade'] ?? 'Desconhecida'), // Adiciona valor padrão caso seja nulo
+          ...menosVenderam.map((c) => c['cidade'] ?? 'Desconhecida') // Adiciona valor padrão caso seja nulo
+        ].toSet().toList(); // Remove duplicados com toSet()
+
+        return List<String>.from(todasCidades);
+      } else {
+        throw Exception('Formato inesperado na resposta da API');
+      }
+    } else {
+      throw Exception('Erro ao buscar cidades');
+    }
+  }
+
+  Future<List<ClienteMaisComprou>> fetchClientesMaisCompraram(int mes) async {
+    final url = 'http://10.0.2.2:3000/clientes-frequentes/$mes';
+    try {
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        final List<dynamic> jsonData = jsonDecode(response.body);
+        return jsonData.map((json) => ClienteMaisComprou.fromJson(json)).toList();
+      } else {
+        print('Erro ao buscar clientes: ${response.statusCode}');
+        throw Exception('Erro ao buscar clientes: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Erro na requisição: $e');
+      rethrow;
+    }
+  }
+
+  Future<List<dynamic>> fetchProdutosMaisVendidosMes(int mes) async {
+    try {
+      final url = Uri.parse("http://10.0.2.2:3000/produtos-mais-vendidos?mes=$mes"); // Usando IP correto
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        final List<dynamic> data = json.decode(response.body);
+        if (data.isNotEmpty) {
+          return data;
+        } else {
+          throw Exception("Nenhum dado encontrado para o mês $mes.");
+        }
+      } else {
+        throw Exception("Erro ao buscar dados: ${response.reasonPhrase} (Código: ${response.statusCode})");
+      }
+    } catch (e) {
+      throw Exception("Erro ao conectar à API: $e");
+    }
+  }
+
+  Future<List<dynamic>> fetchProdutosMaisVendidosPorMes(int mes) async {
+    final response = await http.get(Uri.parse('http://10.0.2.2:3000/produtos-mais-vendidos-mes/$mes'));
+
+    print('Status Code: ${response.statusCode}');
+    print('Resposta da API: ${response.body}');
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return List.from(data); // Converte a resposta em uma lista
+    } else {
+      throw Exception('Erro ao buscar produtos mais vendidos por mês: ${response.body}');
+    }
+  }
+
+  // Função para buscar dados do gráfico de pizza
+  Future<List<double>> fetchPieChartDataByMonth(String month) async {
+    try {
+      final response = await http.get(Uri.parse('http://10.0.2.2:3000/piechart-data/$month'));
       if (response.statusCode == 200) {
         final data = json.decode(response.body);
-        return (data['receitaLiquida'] as num).toDouble(); // Retorna apenas um valor numérico
+        return List<double>.from(data['values']); // Supondo {"values": [40, 30, 15, 15]}
       } else {
-        throw Exception("Erro ao buscar receita líquida: ${response.statusCode}");
+        throw Exception('Erro ao carregar dados do gráfico: ${response.statusCode}');
       }
     } catch (e) {
-      throw Exception("Erro de rede: $e");
+      print('Erro ao buscar dados do gráfico: $e');
+      throw Exception('Erro ao carregar dados do gráfico');
     }
   }
 
-  Future<List<dynamic>> fetchProdutosMaisVendidos() async {
-    final response = await http.get(Uri.parse("$baseUrl/produtos-mais-vendidos"));
-    if (response.statusCode == 200) {
-      return json.decode(response.body);
-    } else {
-      throw Exception("Erro ao buscar produtos mais vendidos");
-    }
-  }
-
-  Future<List<VendaMensal>> fetchVendaMensal() async {
+  // Função para buscar os top produtos no mês
+  Future<List<Map<String, dynamic>>> fetchTopProductsByMonth(String month) async {
     try {
-      final response = await http.get(Uri.parse("$baseUrl/venda-mensal"));
+      final response = await http.get(Uri.parse('http://10.0.2.2:3000/top-products/$month'));
       if (response.statusCode == 200) {
-        List<dynamic> jsonList = json.decode(response.body);
-        return jsonList.map((json) => VendaMensal.fromJson(json)).toList();
+        final data = json.decode(response.body);
+        return List<Map<String, dynamic>>.from(data);
       } else {
-        throw Exception("Erro ao buscar vendas mensais: ${response.statusCode}");
+        throw Exception('Erro ao carregar dados dos produtos mais vendidos');
       }
     } catch (e) {
-      throw Exception("Erro de rede: $e");
+      print('Erro ao buscar dados dos produtos: $e');
+      throw Exception('Erro ao carregar dados dos produtos mais vendidos');
     }
   }
 
-  // Método para buscar produtos mais vendidos na semana
-  Future<List<Produto>> fetchProdutosMaisVendidosSemana() async {
-    final response = await http.get(Uri.parse("$baseUrl/mais-vendidos/semana"));
-    if (response.statusCode == 200) {
-      List<dynamic> jsonList = json.decode(response.body);
-      return jsonList.map((json) => Produto.fromJson(json)).toList();
-    } else {
-      throw Exception("Erro ao buscar produtos mais vendidos na semana: ${response.statusCode}");
-    }
-  }
-
-  // Método para buscar produtos mais vendidos no mês
-  Future<List<dynamic>> fetchTopProdutosMes(String mes) async {
-    final response = await http.get(Uri.parse("$baseUrl/produtos-mais-vendidos-mes/$mes"));
-    if (response.statusCode == 200) {
-      return json.decode(response.body);
-    } else {
-      throw Exception("Erro ao buscar produtos mais vendidos para o mês selecionado");
-    }
-  }
 }
 
 // Modelo de Produto
@@ -97,21 +200,91 @@ class VendaMensal {
 
 // Modelo de ProdutoMaisVendido
 class ProdutoMaisVendido {
-  final String codigoProduto;
   final String descricaoProduto;
-  final int totalVendas;
+  final int totalVendido;
 
   ProdutoMaisVendido({
-    required this.codigoProduto,
     required this.descricaoProduto,
-    required this.totalVendas,
+    required this.totalVendido,
   });
 
   factory ProdutoMaisVendido.fromJson(Map<String, dynamic> json) {
     return ProdutoMaisVendido(
-      codigoProduto: json['codigo_produto'],
-      descricaoProduto: json['descricao_produto'],
-      totalVendas: json['total_vendas'],
+      descricaoProduto: json['descricao_produto'] ?? '', // Garantir que nunca seja null
+      totalVendido: (json['total_vendido'] ?? 0) is int
+          ? json['total_vendido']
+          : (json['total_vendido'] as num?)?.toInt() ?? 0, // Garantir que o valor seja um int
     );
   }
 }
+
+class ClienteMaisComprou {
+  final int idCliente;
+  final String razaoCliente;
+  final double totalCompras;
+
+  ClienteMaisComprou({
+    required this.idCliente,
+    required this.razaoCliente,
+    required this.totalCompras,
+  });
+
+  factory ClienteMaisComprou.fromJson(Map<String, dynamic> json) {
+    return ClienteMaisComprou(
+      idCliente: json['id_cliente'] as int,
+      razaoCliente: json['razao_cliente'] as String,
+      totalCompras: _parseTotalCompras(json['total_compras']),
+    );
+  }
+
+  // Função para tratar a conversão do total_compras
+  static double _parseTotalCompras(dynamic value) {
+    if (value is String) {
+      return double.tryParse(value) ?? 0.0; // Tenta converter a string para double, se não, retorna 0.0
+    } else if (value is num) {
+      return value.toDouble(); // Se já for num (int ou double), converte para double
+    } else {
+      return 0.0; // Caso o valor seja nulo ou de outro tipo inesperado
+    }
+  }
+}
+
+class CidadeVendas {
+  final String cidade;
+  final String uf;
+  final String total_vendas;
+
+  CidadeVendas({
+    required this.cidade,
+    required this.uf,
+    required this.total_vendas,
+  });
+
+  factory CidadeVendas.fromJson(Map<String, dynamic> json) {
+    return CidadeVendas(
+      cidade: json['cidade'],
+      uf: json['uf'],
+      total_vendas: json['total_vendas'],
+    );
+  }
+}
+
+class VendasPorCidade {
+  final String cidade;
+  final double totalVendas;
+
+  VendasPorCidade({required this.cidade, required this.totalVendas});
+
+  factory VendasPorCidade.fromJson(Map<String, dynamic> json) {
+    return VendasPorCidade(
+      cidade: json['cidade'],
+      totalVendas: double.parse(json['total_vendas']),
+    );
+  }
+}
+
+
+
+
+
+
