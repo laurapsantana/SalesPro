f7135bf00981a97216f309ca5bd9fc1c121bd018
Laura Santana <111099291+laurapsantana@users.noreply.github.com>
2024-11-27 23:33:46 -0300

    Atualizando pie_chart.dart
    Atualizando o gráfico de pizza

--- pi_5_semestre_sales_pro/lib/widget/pie_chart.dart
+++ pi_5_semestre_sales_pro/lib/widget/pie_chart.dart
@@ -1,222 +1,81 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-import 'package:pi_5_semestre_sales_pro/resource/app_colors.dart';
-import '../resource/app_padding.dart';
-import 'package:pi_5_semestre_sales_pro/api/api_service.dart';
 
-class PieChartSample2 extends StatefulWidget {
-  const PieChartSample2({super.key});
+class PieChartWidget extends StatefulWidget {
+  final List<Map<String, dynamic>> topProducts;
+  final List<double> values;
+
+  PieChartWidget({required this.topProducts, required this.values});
 
   @override
-  State<StatefulWidget> createState() => PieChart2State();
+  _PieChartWidgetState createState() => _PieChartWidgetState();
 }
 
-class PieChart2State extends State {
-  int touchedIndex = -1;
+class _PieChartWidgetState extends State<PieChartWidget> {
+  final List<Color> colors = [
+    Colors.blue,
+    Colors.red,
+    Colors.green,
+    Colors.orange,
+    Colors.purple,
+    Colors.yellow,
+    Colors.teal,
+    Colors.cyan,
+  ];
+
+  double radius = 80.0;
+  double fontSize = 14.0;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // padding: const EdgeInsets.only(
-      //     left: AppPadding.P10 / 2,
-      //     right: AppPadding.P10 / 2,
-      //     top: AppPadding.P10,
-      //     bottom: AppPadding.P10),
-      child: Card(
-        color: AppColors.purpleLight,
-        elevation: 3,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: AspectRatio(
-          aspectRatio: 1.5,
-          child: Row(
-            children: <Widget>[
-              const SizedBox(
-                height: 18,
-              ),
-              Expanded(
-                child: AspectRatio(
-                  aspectRatio: 1,
-                  child: PieChart(
-                    PieChartData(
-                      pieTouchData: PieTouchData(
-                        touchCallback: (FlTouchEvent event, pieTouchResponse) {
-                          setState(() {
-                            if (!event.isInterestedForInteractions ||
-                                pieTouchResponse == null ||
-                                pieTouchResponse.touchedSection == null) {
-                              touchedIndex = -1;
-                              return;
-                            }
-                            touchedIndex = pieTouchResponse
-                                .touchedSection!.touchedSectionIndex;
-                          });
-                        },
-                      ),
-                      borderData: FlBorderData(
-                        show: false,
-                      ),
-                      sectionsSpace: 0,
-                      centerSpaceRadius: 40,
-                      sections: showingSections(),
-                    ),
-                  ),
-                ),
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const <Widget>[
-                  Indicator(
-                    color: Color(0xff0293ee),
-                    text: 'COPO REQ - MODELO:4F43',
-                    isSquare: true,
-                  ),
-                  SizedBox(
-                    height: 4,
-                  ),
-                  Indicator(
-                    color: Color(0xFFFFC300),
-                    text: 'COPO REQ - MODELO:CX23',
-                    isSquare: true,
-                  ),
-                  SizedBox(
-                    height: 4,
-                  ),
-                  Indicator(
-                    color: Color(0xffff5182),
-                    text: 'CANECA - MODELO:4FFY',
-                    isSquare: true,
-                  ),
-                  SizedBox(
-                    height: 4,
-                  ),
-                  Indicator(
-                    color: Color(0xff13d38e),
-                    text: 'COPO REQ - MODELO:4F43',
-                    isSquare: true,
-                  ),
-                  SizedBox(
-                    height: 18,
-                  ),
-                ],
-              ),
-              const SizedBox(
-                width: 28,
-              ),
-            ],
-          ),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: PieChart(
+        PieChartData(
+          borderData: FlBorderData(show: false),
+          sectionsSpace: 2,
+          centerSpaceRadius: 50,
+          sections: showingSections(),
         ),
       ),
     );
   }
 
   List<PieChartSectionData> showingSections() {
-    return List.generate(4, (i) {
-      final isTouched = i == touchedIndex;
-      final fontSize = isTouched ? 25.0 : 14.0;
-      final radius = isTouched ? 60.0 : 50.0;
-      const shadows = [Shadow(color: Colors.black, blurRadius: 2)];
-      switch (i) {
-        case 0:
-          return PieChartSectionData(
-            color: const Color(0xff0293ee),
-            value: 40,
-            title: '40%',
-            radius: radius,
-            titleStyle: TextStyle(
-              fontSize: fontSize,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-              shadows: shadows,
-            ),
-          );
-        case 1:
-          return PieChartSectionData(
-            color: const Color(0xfff8b250),
-            value: 30,
-            title: '30%',
-            radius: radius,
-            titleStyle: TextStyle(
-              fontSize: fontSize,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-              shadows: shadows,
-            ),
-          );
-        case 2:
-          return PieChartSectionData(
-            color: const Color(0xffff5182),
-            value: 15,
-            title: '15%',
-            radius: radius,
-            titleStyle: TextStyle(
-              fontSize: fontSize,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-              shadows: shadows,
-            ),
-          );
-        case 3:
-          return PieChartSectionData(
-            color: const Color(0xff13d38e),
-            value: 15,
-            title: '15%',
-            radius: radius,
-            titleStyle: TextStyle(
-              fontSize: fontSize,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-              shadows: shadows,
-            ),
-          );
-        default:
-          throw Error();
-      }
-    });
+    List<PieChartSectionData> sections = [];
+
+    for (int i = 0; i < widget.topProducts.length; i++) {
+      // Certifique-se de que o índice não ultrapasse o número de dados
+      if (i >= widget.values.length) break;
+
+      sections.add(PieChartSectionData(
+        color: colors[i % colors.length],  // Cor para cada seção
+        value: widget.values[i], // O valor que será exibido na seção
+        title: '${widget.values[i].toStringAsFixed(1)}%', // Exibe o valor com uma casa decimal
+        radius: radius, // Raio do gráfico
+        titleStyle: TextStyle(
+          fontSize: fontSize,
+          fontWeight: FontWeight.bold,
+          color: Colors.white, // Cor do título
+        ),
+        badgeWidget: _buildBadgeWidget(i),
+      ));
+    }
+
+    return sections;
   }
-}
 
-class Indicator extends StatelessWidget {
-  const Indicator({
-    super.key,
-    required this.color,
-    required this.text,
-    required this.isSquare,
-    this.size = 16,
-    this.textColor = Colors.white70,
-  });
-  final Color color;
-  final String text;
-  final bool isSquare;
-  final double size;
-  final Color? textColor;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Container(
-          width: size,
-          height: size,
-          decoration: BoxDecoration(
-            shape: isSquare ? BoxShape.rectangle : BoxShape.circle,
-            color: color,
-          ),
-        ),
-        const SizedBox(
-          width: 4,
-        ),
-        Text(
-          text,
-          style: TextStyle(
-            fontSize: 13,
-            // fontWeight: FontWeight.bold,
-            color: textColor,
-          ),
-        )
-      ],
+  // Método para construir o badge de cada seção, exibindo o nome do produto
+  Widget _buildBadgeWidget(int i) {
+    // Verifica se há dados para exibir o nome do produto
+    String produtoNome = widget.topProducts[i]['descricao_produto'] ?? 'Produto desconhecido';
+    return Container(
+      padding: EdgeInsets.all(4),
+      color: Colors.white.withOpacity(0.6),
+      child: Text(
+        produtoNome,
+        style: TextStyle(color: Colors.black),
+      ),
     );
   }
 }
